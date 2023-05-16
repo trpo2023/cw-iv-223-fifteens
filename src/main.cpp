93dec7a4566c94b3b775eb4ca60690b818ecb2de
yungkirby616 <mega.nikakoy@bk.ru>
2023-05-16 16:33:20 +0700

    Division of the playground

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,30 @@ int main()
     switch (a)
     {
     case 1:
-        t.loadFromFile("C:/Users/Андрей/Desktop/Paint/Pro/1.png");
+        t.loadFromFile("C:/Users/Данила/Desktop/Paint/Pro/1.png");
         break;
     case 2:
-        t.loadFromFile("C:/Users/Андрей/Desktop/Paint/Pro/2.png");
+        t.loadFromFile("C:/Users/Данила/Desktop/Paint/Pro/2.png");
         break;
     case 3:
-        t.loadFromFile("C:/Users/Андрей/Desktop/Paint/Pro/3.png");
+        t.loadFromFile("C:/Users/Данила/Desktop/Paint/Pro/3.png");
         break;
     case 4:
-        t.loadFromFile("C:/Users/Андрей/Desktop/Paint/Pro/4.png");
+        t.loadFromFile("C:/Users/Данила/Desktop/Paint/Pro/4.png");
         break;
     case 5:
-        t.loadFromFile("C:/Users/Андрей/Desktop/Paint/Pro/5.png");
+        t.loadFromFile("C:/Users/Данила/Desktop/Paint/Pro/5.png");
         break;
     }
+    Sprite s[17];
+    int w = 128;
+    int grid[6][6] = { 0 };
+    int n = 0;
+    for (int i=0; i<4; i++)
+        for (int j = 0; j < 4; j++) {
+            n++;
+            s[n].setTexture(t);
+            s[n].setTextureRect(IntRect(i * w, j * w, w, w));
+            grid[i + 1][j + 1] = n;
+        }
 }
