8422402c2f68c5bc4a081871073176da785187d8
Logashov Danila <logashov-danila@main.ru>
2023-05-23 19:58:52 +0700

    Added tests.cpp

---
+++ src/app/tests.cpp
@@ -0,0 +1,51 @@
+#include <SFML/Graphics.hpp>
+#include <cassert>
+#include <iostream>
+#include "functions.h"
+
+using namespace sf;
+
+// Тестирование функции loadTexture
+void testLoadTexture()
+{
+    Texture t;
+    std::string filename = "Paint/Pro/1.png";
+    loadTexture(t, filename);
+
+    // Проверяем, успешно ли загрузилась текстура
+    assert(t.getSize() != Vector2u(0, 0));
+
+    // Проверяем, что загруженная текстура соответствует ожидаемым размерам
+    assert(t.getSize().x == 512);
+    assert(t.getSize().y == 512);
+
+    std::cout << "TEST: loadTexture SUCCESSFULLY PASSED" << std::endl;
+}
+
+// Тестирование функции drawSprites
+void testDrawSprites()
+{
+    // Создаем окно и спрайты для тестирования
+    RenderWindow window(VideoMode(512, 512), "Tag game!");
+    Sprite s[17];
+    int grid[6][6] = { 0 };
+    Texture t;
+    int w = 128;
+    initializeSprites(s, t, grid);
+
+    // Вызываем функцию drawSprites
+    drawSprites(window, s, grid, w);
+
+    // Проверяем, что спрайты были отрисованы в окне
+
+    std::cout << "TEST: drawSprites SUCCESSFULLY PASSED" << std::endl;
+}
+
+int main()
+{
+    // Тестируем каждую функцию по отдельности
+    testLoadTexture();
+    testDrawSprites();
+
+    return 0;
+}
