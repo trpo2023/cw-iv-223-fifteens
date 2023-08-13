1919bd117752741947e6da1a5b8f54653c3ae509
Logashov Danila <logashov-danila@mail.ru>
2023-08-13 11:39:38 +0700

    Added logical tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -19,33 +19,104 @@ void testLoadTexture()
     assert(t.getSize().x == 512);
     assert(t.getSize().y == 512);
 
-    std::cout << "TEST 1/2 GRAPHIC_TEST:loadTexture [OK]" << std::endl;
+    std::cout << "TEST 1/4 GRAPHIC_TEST:loadTexture [OK]" << std::endl;
 }
 
-// Тестирование функции drawSprites
-void testDrawSprites()
+// Тестирование функции initializeSprites
+void testInitializeSprites()
 {
-    // Создаем окно и спрайты для тестирования
-    RenderWindow window(VideoMode(512, 512), "Tag game!");
     Sprite s[17];
-    int grid[6][6] = { 0 };
     Texture t;
-    int w = 128;
+    int grid[6][6] = { 0 };
     initializeSprites(s, t, grid);
 
-    // Вызываем функцию drawSprites
-    drawSprites(window, s, grid, w);
+    // Проверяем, что в массиве grid все элементы больше нуля
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 4; ++j) {
+            assert(grid[i][j] > 0);
+        }
+    }
 
-    // Проверяем, что спрайты были отрисованы в окне
+    std::cout << "TEST 2/4 GRAPHIC_TEST:initializeSprites [OK]" << std::endl;
+}
 
-    std::cout << "TEST 2/2 GRAPHIC_TEST:drawSprites [OK]" << std::endl;
+// Тестирование завершения игры
+void testGameCompletion()
+{
+    RenderWindow window(VideoMode(512, 512), "Tag game!");
+    Texture t;
+    int a = 1;
+    int w = 128;
+    Sprite s[17];
+    int grid[6][6] = { 0 };
+    initializeSprites(s, t, grid);
+
+    // Расставляем плитки в правильном порядке
+    int num = 1;
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 4; ++j) {
+            grid[i][j] = num++;
+        }
+    }
+
+    // Эмулируем завершение игры (закрытие окна)
+    bool isGameOver = true;
+    window.close();
+
+    // Проверяем, что игра успешно завершена
+    assert(isGameOver);
+
+    std::cout << "TEST 3/4 LOGIC_TEST:gameCompletion [OK]" << std::endl;
+}
+
+// Тестирование алгоритма перемешивания плиток
+void testTileShuffling()
+{
+    RenderWindow window(VideoMode(512, 512), "Tag game!");
+    Texture t;
+    int a = 1;
+    int w = 128;
+    Sprite s[17];
+    int grid[6][6] = { 0 };
+    initializeSprites(s, t, grid);
+
+    // Сохраняем изначальное состояние плиток
+    int initialGrid[6][6];
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 4; ++j) {
+            initialGrid[i][j] = grid[i][j];
+        }
+    }
+
+    // Перемешиваем плитки
+    shuffleTiles(grid);
+
+    // Проверяем, что хотя бы одна плитка изменила свою позицию
+    bool isShuffled = false;
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 4; ++j) {
+            if (grid[i][j] != initialGrid[i][j]) {
+                isShuffled = true;
+                break;
+            }
+        }
+        if (isShuffled) {
+            break;
+        }
+    }
+
+    // Проверяем, что плитки были перемешаны
+    assert(isShuffled);
+
+    std::cout << "TEST 4/4 LOGIC_TEST:tileShuffling [OK]" << std::endl;
 }
 
 int main()
 {
-    // Тестируем каждую функцию по отдельности
     testLoadTexture();
-    testDrawSprites();
+    testInitializeSprites();
+    testGameCompletion();
+    testTileShuffling(); 
 
     return 0;
 }
