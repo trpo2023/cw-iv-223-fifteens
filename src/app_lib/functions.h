0c1bd6cb93d1c4c373b002a1c2715b7be07601b8
Logashov Danila <logashov-danila@mail.ru>
2023-08-13 11:46:11 +0700

    Fixed functions.h

--- src/app_lib/functions.h
+++ src/app_lib/functions.h
@@ -5,8 +5,16 @@
 
 void loadTexture(sf::Texture& t, const std::string& filename);
 void initializeSprites(sf::Sprite s[], sf::Texture& t, int grid[][6]);
-void handleMouseClick(sf::Event::MouseButtonEvent mouseEvent, sf::RenderWindow& window, int grid[][6], sf::Sprite s[], int a, int w);
-void handleKeyPress(sf::Keyboard::Key key, int grid[][6], sf::Sprite s[], int &a, int w);
-void drawSprites(sf::RenderWindow& window, sf::Sprite s[], int grid[][6], int w);
-void shuffleTiles(int grid[][6]); 
+void handleMouseClick(
+        sf::Event::MouseButtonEvent mouseEvent,
+        sf::RenderWindow& window,
+        int grid[][6],
+        sf::Sprite s[],
+        int a,
+        int w);
+void handleKeyPress(
+        sf::Keyboard::Key key, int grid[][6], sf::Sprite s[], int& a, int w);
+void drawSprites(
+        sf::RenderWindow& window, sf::Sprite s[], int grid[][6], int w);
+void shuffleTiles(int grid[][6]);
 #endif
